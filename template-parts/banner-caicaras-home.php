2a4245b68e9ca06b038e3d944ed42c1c9e1b9dd0
JucaBonini <jucasouzabonini@gmail.com>
2026-05-04 21:27:23 -0300

    fix: corrigir nome da imagem webp no banner

--- template-parts/banner-caicaras-home.php
+++ template-parts/banner-caicaras-home.php
@@ -9,7 +9,7 @@
     .banner-caicaras-home {
         position: relative;
         padding: 80px 0;
-        background: url('<?php echo get_template_directory_uri(); ?>/assets/images/fundo-banner-ca.webp') no-repeat center center;
+        background: url('<?php echo get_template_directory_uri(); ?>/assets/images/fundo-banner-caicara.webp') no-repeat center center;
         background-size: cover;
         overflow: hidden;
         color: white;
